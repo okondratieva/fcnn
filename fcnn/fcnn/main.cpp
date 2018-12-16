7be99e1c59872d79a2af82be3f2936ddcd6af663
okondratieva <olga-kondratieva@inbox.ru>
2018-12-16 03:21:54 +0300

    fill the skeleton

--- fcnn/fcnn/main.cpp
+++ fcnn/fcnn/main.cpp
@@ -1,6 +1,9 @@
 
+#include <string>
 #include <iostream>
 #include <ctime>
+#include <vector>
+#include <algorithm>
 
 #define USE_MNIST_LOADER
 #define MNIST_FLOAT
@@ -19,18 +22,57 @@ int main(int argc, char **argv)
 
 	mnist_data *train;
 	mnist_data *test;
-	unsigned int cnt_train;
-	unsigned int cnt_test;
-
-	mnist_load("train-images.idx3-ubyte", "train-labels.idx1-ubyte", &train, &cnt_train);
-	mnist_load("t10k-images.idx3-ubyte", "t10k-labels.idx1-ubyte", &test, &cnt_test);
-
+	unsigned int cntTrain;
+	unsigned int cntTest;
+	
 	float learningRate = atof(argv[1]);
-	unsigned int epoch = atoi(argv[2]);
+	unsigned int epochCount = atoi(argv[2]);
+	unsigned int hiddenSize = atoi(argv[3]);
+	string pathToData(argv[4]);
+	unsigned int imageSize = 28 * 28;
 
-	FCNetwork network();
+	cout << "Parameters: " << endl;
+	cout << "learning rate: " << learningRate<<endl;
+	cout << "epochs: " << epochCount << endl;
+	cout << "hidden neurons: " << hiddenSize << endl;
+	cout << "path to data: " << pathToData << endl;
 
-	//train procedures 
+	//TODO: string to char
 
+	mnist_load("train-images.idx3-ubyte", "train-labels.idx1-ubyte", &train, &cntTrain);
+	mnist_load("t10k-images.idx3-ubyte", "t10k-labels.idx1-ubyte", &test, &cntTest);
+
+
+	FCNetwork network(imageSize, hiddenSize, 0.1);
+	vector<unsigned int> indexes; //for suffle procedure
+	indexes.resize(cntTrain);
+
+	//train and test procedures 
+	for (unsigned int i = 0; i < cntTrain; ++i)
+		indexes[i] = i;
+
+	for (unsigned int epoch = 0; epoch < epochCount; epoch++)
+	{
+		random_shuffle(indexes.begin(), indexes.end());
+		for (unsigned int i = 0; i < cntTrain; ++i)
+		{
+			network.set(train[indexes[i]].data);
+			network.teach(train[indexes[i]].label, learningRate);
+			if (i % 100 == 0)
+				cout << "\repoch: " << epoch << " progress: " << (i / double(cntTrain)) * 100 << "%         ";
+		}
+	}
+
+	unsigned int accuracy = 0;
+	int res;
+	for (unsigned int i = 0; i < cntTest; ++i)
+	{
+		network.set(test[i].data);
+		res = network.test();
+		if (res == test[i].label)
+			++accuracy;
+	}
+
+	cout << "\raccuracy: " << (double(accuracy) / cntTest) * 100 << "%          " << endl;
 	return 0;
 }
--- fcnn/fcnn/network.h
+++ fcnn/fcnn/network.h
@@ -12,30 +12,47 @@ float random()
 
 class FCNetwork {
 public:
-	unsigned int sizes[3];//sizes of layers' output
-	float *weights[2]; //weights of connections between 0-1 and 1-2 layers
-	float *outs[3]; //outputs of layers, 0 for network's input
-	int k = 10; //count of neurons in the second layer
+	unsigned int sizes[3];	//sizes of layers' output
+	float *weights[2];		//weights of connections between 0-1 and 1-2 layers
+	float *outs[3];			//outputs of layers, 0 for network's input
+	float *derivatives[2];	//derivatives for updating weights
+	int k = 10;				//count of neurons in the second layer
+	float eps = 0.1;		//error 
 
 public:
-	FCNetwork(int size, int hiddenLayerSize) {
-		
-		sizes[0] = size;
-		sizes[1] = hiddenLayerSize * size;
-		sizes[2] = hiddenLayerSize * k;
+	FCNetwork(int size, int hiddenLayerSize, int accuracy) {
+
+		eps = accuracy;
+
+		sizes[0] = size; //input verctor
+		sizes[1] = hiddenLayerSize;
+		sizes[2] = k;
 
 		outs[0] = new float[size];
-		weights[0] = new float[sizes[1]];
-		weights[1] = new float[sizes[2]];
+		outs[1] = new float[sizes[1]];
+		outs[2] = new float[sizes[2]];
 
-		for (int i = 0; i < sizes[1]; i++) {
+		derivatives[0] = new float[sizes[1]]; //for output from first layer
+		derivatives[1] = new float[sizes[2]]; //for output from softmax layer
+
+		weights[0] = new float[sizes[0]*sizes[1]];
+		weights[1] = new float[sizes[1]*sizes[2]];
+
+		for (int i = 0; i < sizes[0] * sizes[1]; i++) {
 			weights[0][i] = random();
 		}
 
-		for (int i = 0; i < sizes[2]; i++) {
+		for (int i = 0; i < sizes[1] * sizes[2]; i++) {
 			weights[1][i] = random();
 		}
-		
+
+		for (int i = 0; i < sizes[1]; i++) {
+			outs[1][i] = 0.0f;
+		}
+
+		for (int i = 0; i < sizes[2]; i++) {
+			outs[2][i] = 0.0f;
+		}
 	}
 
 	void set(float *inputs) {
@@ -53,20 +70,16 @@ public:
 		int value = 0;
 		for (int i = 0; i < sizes[2]; i++)
 			if (outs[2][i] > max) {
-				max = outs[2][i];//to find prediction class
-				value = i;
+				max = outs[2][i];  //to find predicted class
+				value = i;			//predicted class
 			}
 		return value;
-	}
-
-	void teach() {
-
 	}
 
 	//activation function for the first layer (hidden layer)
 	void activation() {
 		for (int i = 0; i < sizes[1]; i++)
-			weights[0][i] = tanh(weights[0][i]);
+			outs[1][i] = tanh(outs[1][i]);
 	}
 
 	//activation function for the second layer (softmax function)
@@ -81,5 +94,73 @@ public:
 			outs[2][i] = expf(outs[2][i])*sum;
 	}
 
+	void updateDerivatives(int label)
+	{
+		//for softmax layer
+		for (int i = 0; i < sizes[2]; i++)
+		{
+			for (unsigned int j = 0; j < sizes[2]; j++)
+				derivatives[1][i] = outs[2][j];
+			derivatives[1][label] -= 1.f;
+		}
+		
+		//for hidden layer (with tanh activation function)
+		for (int i = 0; i < sizes[1]; i++)
+		{
+			derivatives[0][i] = 0.f;
+			for (unsigned int j = 0; j < sizes[2]; j++)
+				derivatives[0][i] += derivatives[1][j] * weights[1][j*sizes[2] + i];
+			derivatives[0][i] *= 1.f - outs[1][i] * outs[1][i];
+		}		
+	}
 
+	void directStep() {
+		//outs after first layer (hidden)
+		for (int i = 0; i < sizes[1]; i++) {
+			for (int j = 0; j < sizes[0]; j++) {
+				outs[1][i] += weights[0][i*sizes[0] + j] * outs[0][j];
+			}
+			outs[1][i] = tanh(outs[1][i]);
+		}
+
+		//outs after second layer (softmax)
+		float sum = 0.f;
+		for (int i = 0; i < sizes[2]; i++) {
+			for (int j = 0; j < sizes[1]; j++) {
+				outs[1][i] += weights[1][i*sizes[1] + j] * outs[1][j];
+			}
+			sum += expf(outs[2][i]);
+		}
+		sum = 1.f / sum;
+		for (int i = 0; i < sizes[2]; i++)
+			outs[2][i] = expf(outs[2][i])*sum;
+	}
+
+	void backPropagation(float learningRate, int label) {
+		//calculate derivatives
+		updateDerivatives(label);
+		
+		//update weights
+		for (int i = 0; i < sizes[2]; ++i) {
+			for (unsigned int j = 0; j < sizes[1]; j++) {
+				weights[1][i*sizes[1] + j] -= derivatives[2][i] * outs[1][j] * learningRate;
+			}
+		}
+
+		for (int i = 0; i < sizes[1]; ++i) {
+			for (unsigned int j = 0; j < sizes[0]; j++) {
+				weights[1][i*sizes[0] + j] -= derivatives[2][i] * outs[0][j] * learningRate;
+			}
+		}
+	}
+
+	void teach(float learningRate, int label) {
+		directStep();
+		backPropagation(learningRate, label);
+	}
+
+	int test() {
+		directStep();
+		return predict();
+	}
 };
